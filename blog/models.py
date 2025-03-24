d74032dbda03c25c3013dd2849fdeaabe6214664
Eyongtarh Besong <eyongtarhb@yahoo.co.uk>
2025-03-24 09:17:03 +0100

    add excerpt and update_on fields to Post model including migrations and migrate

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-STATUS = ((0, "Draft"), (1, "Published"))
-
 # Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Post(models.Model):
@@ -15,3 +14,5 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
