e22c371d09bc03b4ad82ff00fce46626badbded8
Eyongtarh Besong <eyongtarhb@yahoo.co.uk>
2025-04-21 18:51:48 +0200

    edit cloudinary in env.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-mfg8(l6&1lr&v*5+7!zzs058^uni&)=%m^yw)midf0@z#aqw3y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
